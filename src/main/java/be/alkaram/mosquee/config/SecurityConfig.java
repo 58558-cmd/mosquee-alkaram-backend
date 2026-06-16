215fd2568427d51c659a8cfb721edfa4bf11ccc4
58558 <58558@etu.he2b.be>
2026-06-16 20:53:09 +0200

    Fix CORS for production

--- src/main/java/be/alkaram/mosquee/config/SecurityConfig.java
+++ src/main/java/be/alkaram/mosquee/config/SecurityConfig.java
@@ -51,7 +51,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "http://localhost:8080"));
+                "http://localhost:8080",
+                "https://mosquee-alkaram-frontend-production.up.railway.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
